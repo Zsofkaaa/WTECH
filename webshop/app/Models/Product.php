b3748c03ebb57d96ac06b349811a05b23e69b92e
Flóra Emma Kaňuchová <145666177+knchflora@users.noreply.github.com>
2025-04-25 00:18:21 +0200

    Add files via upload

--- webshop/app/Models/Product.php
+++ webshop/app/Models/Product.php
@@ -2,30 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-
-
 class Product extends Model
 {
+    use HasFactory;
     protected $fillable = [
-        'name', 'description', 'price', 'min_age',
-        'min_players', 'max_players', 'category',
-        'is_new', 'is_best_seller', 'is_discounted'
+        'name',
+        'description',
+        'price',
+        'discounted_price',
+        'is_discounted',
+        'is_best_seller',
+        'is_new',
+        'min_age',
+        'min_players',
+        'max_players',
     ];
-
-
 
     public function images()
     {
         return $this->hasMany(ProductImage::class);
     }
 
-
-
     public function getMainImagePath()
     {
         return asset('Pictures/' . Str::slug($this->name) . '1.jpg');
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'category_product');
+    }
+
+    public function scopeDiscounted($query)
+    {
+        return $query->whereNotNull('discounted_price');
+    }
+   
 }
