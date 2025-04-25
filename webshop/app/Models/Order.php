248ae07d4f516a6521ddc3e601c50f7f3634849d
Flóra Emma Kaňuchová <145666177+knchflora@users.noreply.github.com>
2025-04-25 03:41:41 +0200

    Add files via upload

--- webshop/app/Models/Order.php
+++ webshop/app/Models/Order.php
@@ -4,14 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
-
 class Order extends Model
 {
     protected $fillable = [
         'full_name', 'email', 'phone', 'address',
         'delivery_method', 'payment_method', 'total_price'
     ];
+
 
     public function products()
     {
