7438358002f62573cfaa4fcdc3e40dfbf14b893e
Zsófia Gergely <xgergelyz@stuba.sk>
2025-05-10 23:41:24 +0200

    Add files via upload

--- webshop/app/Http/Controllers/SearchController.php
+++ webshop/app/Http/Controllers/SearchController.php
@@ -21,29 +21,52 @@ class SearchController extends Controller
     {
         $query = $request->input('query');
         $normalizedQuery = strtolower($this->removeAccents($query));
-        $productsCollection = Product::all()->filter(function ($product) use ($normalizedQuery) {
+
+        $sort = $request->query('sort', 'asc');
+
+        $productsCollection = Product::with('images')->get()->filter(function ($product) use ($normalizedQuery) {
             $normalizedName = strtolower($this->removeAccents($product->name));
-            return str_contains($normalizedName, $normalizedQuery);
+
+            $effectivePrice = $product->is_discounted && $product->discounted_price
+                ? $product->discounted_price
+                : $product->price;
+
+            if (!str_contains($normalizedName, $normalizedQuery)) {
+                return false;
+            }
+
+            $product->effective_price = $effectivePrice;
+            return true;
         });
+
+        if ($sort === 'price_asc') {
+            $productsCollection = $productsCollection->sortBy('effective_price')->values();
+        } elseif ($sort === 'price_desc') {
+            $productsCollection = $productsCollection->sortByDesc('effective_price')->values();
+        } elseif ($sort === 'desc') {
+            $productsCollection = $productsCollection->sortByDesc('name')->values();
+        } else {
+            $productsCollection = $productsCollection->sortBy('name')->values();
+        }
+
         $perPage = 12;
-        $currentPage = request()->get('page', 1);
+        $currentPage = $request->input('page', 1);
         $currentItems = $productsCollection->slice(($currentPage - 1) * $perPage, $perPage)->values();
+
         $products = new LengthAwarePaginator(
             $currentItems,
             $productsCollection->count(),
             $perPage,
             $currentPage,
-            ['path' => request()->url(), 'query' => request()->query()]
+            ['path' => $request->url(), 'query' => $request->query()]
         );
 
         $categoryTitle = "Výsledky hľadania pre: \"$query\"";
 
         return view('shop', [
             'products' => $products,
-            'categoryTitle' => $categoryTitle
+            'categoryTitle' => $categoryTitle,
+            'sort' => $sort,
         ]);
     }
-
 }
-
-
